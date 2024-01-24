1bdf96e971875339e7224087a989923ffd0124fa
Lasha <bg.7788211@gmail.com>
2024-01-24 16:55:30 +0300

    add Lesson2:task1

---
+++ Desktop/CSharp/Lesson2/task1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+// Console.WriteLine(arr[5]);
+
+// Способ заполнения массива при его создании, 
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+// Упрощенный синтаксис для создания массива, команду на выделение памяти
+// в некоторых случаях можно опустить
+int[] arr3 = { 1, 2, 3, 4, 5 };
+
+// arr = [1,2,3,4,5];  -  Синтаксис создания массива на языке Python
