8df8994f44b9b3572c62500ad423ff16412f4575
Lasha <bg.7788211@gmail.com>
2024-01-30 21:51:43 +0300

    add Lesson3:task3

---
+++ Desktop/CSharp/Lesson3/task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void ZeroEventElements(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+
+int[] array = {15,16,3,55,54,35};
+ZeroEventElements(array);
+PrintArray(array);
